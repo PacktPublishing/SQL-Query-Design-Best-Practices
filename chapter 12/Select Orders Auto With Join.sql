b095e870ac041780605b76081dee4a5149142d7f
Steve Hughes <steve@dataonwheels.com>
2023-02-07 11:29:32 -0500

    update from TR review

--- chapter 12/Select Orders Auto With Join.sql
+++ chapter 12/Select Orders Auto With Join.sql
@@ -1,9 +1,9 @@
 
 SELECT TOP (3) 
-      c.CustomerID as customer.id
-      , c.CustomerName as customer.name
-     , o.OrderID as customer.order.id
-      , o.OrderDate as customer.order.orderdate
+      c.CustomerID as 'customer.id'
+      , c.CustomerName as 'customer.name'
+     , o.OrderID as 'customer.order.id'
+      , o.OrderDate as 'customer.order.orderdate'
 FROM Sales.Customers c
 INNER JOIN Sales.Orders o ON c.CustomerID = o.CustomerID
 WHERE o.CustomerID = 2 AND YEAR(o.OrderDate) = 2016
